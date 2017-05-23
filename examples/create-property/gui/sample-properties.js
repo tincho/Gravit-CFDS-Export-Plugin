17de61f7b18d29c24a60a3098227da7c432af4c1
Denner Erthal <dennererthal00@gmail.com>
2017-05-23 00:44:03 -0300

    Fixed properties example

--- examples/create-property/gui/sample-properties.js
+++ examples/create-property/gui/sample-properties.js
@@ -3,6 +3,8 @@ var GRectangle = _core.GRectangle;
 var GStylable = _core.GStylable;
 var GRGBColor = _core.GRGBColor;
 var GElement = _core.GElement;
+var GObject = _core.GObject;
+var GProperties = GravitDesigner.GProperties;
 
 /**
  * A sample properties panel
@@ -12,6 +14,7 @@ var GElement = _core.GElement;
  */
 function GSampleProperties() {
 };
+GObject.inherit(GSampleProperties, GProperties);
 
 /**
  * @type {JQuery}
@@ -31,8 +34,8 @@ GSampleProperties.prototype._toolbar = null;
  * @param {JQuery} toolbar the optional toolbar to init on
  */
 GSampleProperties.prototype.init = function (panel, toolbar) {
-    this._toolbar = toolbar;
-    this._panel = panel;
+    this._toolbar = toolbar.addClass('sample-properties-toolbar');
+    this._panel = panel.addClass('sample-properties-panel');
 
     //add a title to the toolbar
     $('<label></label>')
@@ -44,40 +47,47 @@ GSampleProperties.prototype.init = function (panel, toolbar) {
       .gPropertyRow({
           columns: [
               {
-                  width: '25%',
+                  width: '50%',
                   content: $('<button/>')
                     .addClass('add-rectangle')
                     .html('Add rectangle')
                     .on('click', function() {
                         this._addRectangle();
-                    }.bind(this));
+                    }.bind(this))
               },
               {
-                  width: '25%',
+                  width: '50%',
                   content: $('<button/>')
                     .addClass('select-all')
                     .html('Select all')
                     .on('click', function() {
                         this._selectAll();
-                    }.bind(this));
-              },
+                    }.bind(this))
+              }
+          ]
+      })
+      .appendTo(this._panel);
+
+    $('<div></div>')
+      .gPropertyRow({
+          columns: [
               {
-                  width: '25%',
+                  width: '50%',//size of the content
                   content: $('<button/>')
                     .addClass('clear-selection')
                     .html('Clear selection')
                     .on('click', function() {
                         this._clearSelection();
-                    }.bind(this));
+                    }.bind(this))
               },
               {
-                  width: '25%',
+                  width: '50%',
                   content: $('<button/>')
                     .addClass('delete-all')
                     .html('Delete Selection')
                     .on('click', function() {
                         this._deleteSelection();
-                    }.bind(this));
+                    }.bind(this))
               }
           ]
       })
@@ -105,18 +115,25 @@ GSampleProperties.prototype.update = function (document, elements) {
     var selection = gDesigner.getActiveDocument().getEditor().getSelection();
     var hasSelection = selection && selection.length > 0;
 
+    //example to disable buttons depending on the selection
     this._panel.find('.clear-selection').prop('disabled', !hasSelection);
     this._panel.find('.delete-all').prop('disabled', !hasSelection);
+
+    return true;
 };
 
 /**
  * Create a new rectangle, paint it and add it to the current page
  */
 GSampleProperties.prototype._addRectangle = function() {
+    //instantiate a new rectangle using the GRectangle class of the framework
+    //it will be added in the position x: 0, y:0, with width: 50, height: 50
     var rectangle = new GRectangle(0, 0, 50, 50);
 
+    //elements by default are created without fill, so lets add one FillPaintLayer from GStylable
     rectangle.getPaintLayers().appendChild(new GStylable.FillPaintLayer(new GRGBColor([235,235,235]), 1.0));
 
+    //finally, lets add the new element to the active page
     gDesigner.getActiveDocument().getEditor().getScene().getActivePage().appendChild(rectangle);
 };
 
@@ -126,12 +143,16 @@ GSampleProperties.prototype._addRectangle = function() {
 GSampleProperties.prototype._selectAll = function() {
     var newSelection = [];
 
+    //iterate through the elements
     gDesigner.getActiveDocument().getEditor().getScene().getActivePage().acceptChildren(function (node) {
+        //the FullLocked flag means that the element cannot be selected
         if (!node.hasFlag(GElement.Flag.FullLocked)) {
+            //add the element to new selection
             newSelection.push(node);
         }
     }, false, true);
 
+    //update the selection
     gDesigner.getActiveDocument().getEditor().updateSelection(false, newSelection);
 };
 
@@ -139,6 +160,7 @@ GSampleProperties.prototype._selectAll = function() {
  * Clear selection
  */
 GSampleProperties.prototype._clearSelection = function() {
+    //to clear the selection, we can update it using a empty array
     gDesigner.getActiveDocument().getEditor().updateSelection(false, []);
 };
 
@@ -146,6 +168,7 @@ GSampleProperties.prototype._clearSelection = function() {
  * Delete selection
  */
 GSampleProperties.prototype._deleteSelection = function() {
+    //delete selection
     gDesigner.getActiveDocument().getEditor().deleteSelection();
 };
 

b5367d3bb90b15cfbf07e6520001e6481e69b4c2
Félix Desjardins <FelixINX@users.noreply.github.com>
2017-07-09 13:32:41 -0400

    Removed examples

--- examples/loading-html/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    init: function (gravit) {},
-    start: function () {
-    	$.get(process.cwd() + "/src/template.html", function(data) {
-	    	$($('.file-section .toolbar-button')[0]).after(data);
-	  	});
-    }
-};
